c214525b8f4901a6368fe1cb92551f5db6dfc404
Lunny Xiao <xiaolunwen@gmail.com>
2013-12-15 11:51:20 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 config
 ======
+
+Config is a simple config package for parse and save config files.
+
+The config format like below:
+
+```
+a=b
+c=d
+```
---
+++ config_test.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+    "testing"
+)
+
+func TestParse(t *testing.T) {
+    cfgs, err := Load("./test_cfg.ini")
+    if err != nil {
+        t.Error(err)
+    }
+
+    if cfgs.Has("no_exist_key") {
+        t.Errorf("no exist key error")
+    }
+
+    if v := cfgs.Get("dbhost"); len(v) > 0 {
+        t.Errorf("key dbhost should be empty")
+    }
+
+    if v, _ := cfgs.GetBool("usecache"); v {
+        t.Errorf("key usecache should be false")
+    }
+
+    if v, _ := cfgs.GetInt("mgrPort"); v != 8866 {
+        t.Errorf("key mgrPort should be int and equal 8866")
+    }
+}
